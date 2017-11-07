a9350fe33cb7231a33dbf4f1c50c56414c8b5567
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-11-06 21:23:34 -0500

    small changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
 
   def self.search(search)
-    find_by_sql("select title,description,url from books where title LIKE '#{search}'")
+    find_by_sql("select title,description,url from books where title LIKE '%#{search}%'")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "statics#index"
+  get '/security', to: "statics#index"
   get '/security/search', to: "statics#search"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
